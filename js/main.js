87eff91e00d693f61be3779020bb4b8dced9103e
vishnukhatik <khatikvishnu7253@gmail.com>
2020-05-27 20:06:46 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$(".menuIconWidth").click(function(){
+    $(".menu").removeClass("d-none");
+  });
+  $(".overlay-close").click(function(){
+    $(".menu").addClass("d-none");
+  });
+}
